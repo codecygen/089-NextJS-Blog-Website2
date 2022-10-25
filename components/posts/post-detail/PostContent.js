c776f325e12d1e29bca2dc1b2a9431495c071d11
gingercook <vahitaras@gmail.com>
2022-10-24 20:08:53 -0400

    end of day

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -4,15 +4,6 @@ import PostHeader from './PostHeader';
 
 import classes from './PostContent.module.css';
 
-const DUMMY_POST = {
-  slug: 'new-blog-post',
-  title: 'New Blog Post4',
-  image: 'new-blog-post.png',
-  exerpt: 'This will be my first post in this website!',
-  date: '2022-10-08',
-  content: '# This is the first post!' 
-};
-
 const PostContent = (props) => {
   const { post } = props;
 
