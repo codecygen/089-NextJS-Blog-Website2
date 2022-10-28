5c404a70f92dfb7080cfe23b94bfc712c68e0a64
gingercook <vahitaras@gmail.com>
2022-10-27 22:55:26 -0400

    contact form can send message

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -1,28 +1,84 @@
+import { useState } from 'react';
+
 import classes from './ContactForm.module.css';
 
 const ContactForm = () => {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredMessage, setEnteredMessage] = useState('');
+
+    const sendMessageHandler = (e) => {
+        e.preventDefault();
+
+        // Can have client side entered data validation
+        // we already put required but still there could be
+        // validation.
+
+        // fetch('/api/contact', {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         email: enteredEmail, 
+        //         name: enteredName, 
+        //         message: enteredMessage
+        //     }), 
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // });
+
+        fetch('/api/contact', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: enteredEmail, 
+                name: enteredName, 
+                message: enteredMessage
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    };
+
     return (
         <section className={classes.contact}>
             <h1>How can I help you?</h1>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={sendMessageHandler}>
                 <div className={classes.controls}>
                     <div className={classes.control}>
                         <label htmlFor="email">Your Email</label>
-                        <input type="email" id="email" />
+                        <input
+                            type="email"
+                            id="email" 
+                            required
+                            value={enteredEmail}
+                            onChange={e => setEnteredEmail(e.target.value)}
+                        />
                     </div>
                 </div>
 
                 <div className={classes.controls}>
                     <div className={classes.control}>
                         <label htmlFor="name">Your Name</label>
-                        <input type="text" id="name" />
+                        <input
+                            type="text"
+                            id="name" 
+                            required
+                            value={enteredName}
+                            onChange={e => setEnteredName(e.target.value)}
+                        />
                     </div>
                 </div>
 
                 <div className={classes.controls}>
                     <div className={classes.control}>
                         <label htmlFor="message">Your Message</label>
-                        <textarea id="message" rows="5" />
+                        <textarea
+                            id="message"
+                            rows="5" 
+                            required
+                            value={enteredMessage}
+                            onChange={e => setEnteredMessage(e.target.value)}
+                        />
                     </div>
                 </div>
 
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,29 +1,30 @@
-const ContactSubmitHandler = (req, res) => {
+const contactSubmitHandler = (req, res) => {
     const { email, name, message } = req.body;
 
     if (req.method === 'POST') {
         if (
             !email ||
-            email.includes('@') ||
+            !email.includes('@') ||
             !name || name.trim() === '' ||
             !message || message.trim === ''
         ) {
-            res.status(422).json({ message: 'Invalud input!' });
+            console.log('aras');
+            res.status(422).json({ message: 'Invalid input!' });
             return;
         }
+
+        // Store it in a database
+        const newMessage = {
+            email,
+            name,
+            message
+        }
+
+        console.log(newMessage)
+
+        res.status(201).json({ message: 'Successfully stored message!', sentMessage: newMessage });
     }
-
-    // Store it in a database
-    const newMessage = {
-        email, 
-        name, 
-        message
-    }
-
-    console.log(newMessage)
-
-    res.status(201).json({ message: 'Successfully stored message!', sentMessage: newMessage });
 
 };
 
-export default ContactSubmitHandler;
+export default contactSubmitHandler;
